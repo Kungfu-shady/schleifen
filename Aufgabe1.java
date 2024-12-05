6386968d2ad721e6ee488dac534325b06896cbd4
Kungfu-shady <fernando.alexperezav@gmail.com>
2024-12-05 10:47:02 +0100

    copy paste

--- Aufgabe1.java
+++ Aufgabe1.java
@@ -1,13 +1,1 @@
-public class Aufgabe1 {
- public static void main(String[] args){
- int a = 0;
- int b = 0;
- while (a < 10) {
- ++a;
- b = b + a;
- }
- System.out.println(a);
- System.out.println(b);
- }
-}
 
